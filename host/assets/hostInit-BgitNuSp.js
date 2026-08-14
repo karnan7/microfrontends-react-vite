fcb8699e248806eafae9eba60f7796d5e9291c03
karnan7 <karnan7@users.noreply.github.com>
2026-08-14 20:34:38 +0000

    deploy: d83da5c73828e6c46769ab8e0b2b21826626fd00

---
+++ host/assets/hostInit-BgitNuSp.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./_virtual_mf___mfe_internal__host__mf_owner__1__H_A_I__hostAutoInit__H_A_I__-I7QF8R6y.js";export{t as hostInitPromise,e as initHost};
---
+++ host/assets/remoteEntry-D8J9OBxq.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./virtual_mf-REMOTE_ENTRY_ID___mfe_internal__host__remoteEntry-_hash_-o11VegFU.js";export{t as get,e as init};
